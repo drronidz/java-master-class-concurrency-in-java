db021720ff33ed8bb246cb595ee356c11bc0536b
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-28 00:20:24 +0100

    issue done. closes #9

--- Messages/src/com/drronidz/Main.java
+++ Messages/src/com/drronidz/Main.java
@@ -9,7 +9,9 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
 
@@ -19,17 +21,27 @@ class Message {
 
     public synchronized String read() {
         while (empty) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         empty = true;
+        notifyAll();
         return message;
     }
 
     public synchronized void write(String message) {
         while (!empty) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         empty = false;
+        notifyAll();
         this.message = message;
     }
 }
@@ -54,11 +66,33 @@ class Writer implements Runnable {
         for (int i = 0; i < messages.length; i++) {
             message.write(messages[i]);
             try {
-                Thread.sleep(random.nextInt(2000));
+                Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for(String latestMessage = message.read();
+            !latestMessage.equals("Finished");
+            latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
