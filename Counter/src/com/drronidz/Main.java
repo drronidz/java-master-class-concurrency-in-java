813c216f9a8111bf2bd0e3762b88ee849e5bb183
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-27 23:28:33 +0100

    issue done. closes #7

--- Counter/src/com/drronidz/Main.java
+++ Counter/src/com/drronidz/Main.java
@@ -38,9 +38,10 @@ class Countdown {
             default:
                 color = ThreadColor.ANSI_GREEN;
         }
-
-        for( i=10; i > 0; i--) {
-            System.out.println(color + Thread.currentThread().getName() + ": i =" + i);
+        synchronized (this) {
+            for( i=10; i > 0; i--) {
+                System.out.println(color + Thread.currentThread().getName() + ": i =" + i);
+            }
         }
     }
 }
