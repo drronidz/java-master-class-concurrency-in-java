89d0ba29eb43aa087bb9a7912fb836acf9b03487
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-29 01:25:16 +0100

    issue done. closes #20

--- PoliteWorker/src/com/drronidz/Main.java
+++ PoliteWorker/src/com/drronidz/Main.java
@@ -7,7 +7,25 @@ package com.drronidz;/*
 
 public class Main {
     public static void main(String[] args) {
+
         final Worker workerOne = new Worker("Worker 1", true);
         final Worker workerTwo = new Worker("Worker 2", true);
+
+        final SharedResource sharedResource = new SharedResource(workerOne);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                workerOne.work(sharedResource, workerTwo);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                workerTwo.work(sharedResource, workerOne);
+            }
+        }).start();
+
     }
 }
