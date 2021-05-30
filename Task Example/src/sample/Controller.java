ea00cf70c40b23701825eab679084c7d102edc77
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-31 00:50:37 +0100

    issue done. closes #23

--- Task Example/src/sample/Controller.java
+++ Task Example/src/sample/Controller.java
@@ -7,7 +7,9 @@ import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +19,12 @@ public class Controller implements Initializable {
     @FXML
     public ListView listView;
 
+    @FXML
+    public ProgressBar progressBar;
+
+    @FXML
+    public Label progressLabel;
+
     private Task<ObservableList<String>> task;
 
     @Override
@@ -24,20 +32,29 @@ public class Controller implements Initializable {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
-                final ObservableList<String> employees = FXCollections.observableArrayList(
+                String[] names = {
                         "DIAB ABDOU",
                         "OMOR GUEMMAZ",
                         "DIALO ABOU",
-                        "DJABAIR DJILALI"
-                );
-
+                        "DJABAIR DJILALI",
+                        "BAARAOUI MAKHLOUF",
+                        "MAKHLOUFI MOKHTAR",
+                };
+                ObservableList<String> employees = FXCollections.observableArrayList();
 
 //                Platform.runLater(() -> listView.setItems(employees));
+
+                for(int i=0; i<6; i++) {
+                    employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, 6);
+                    Thread.sleep(200);
+                }
                 return employees;
             }
         };
-
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
     @FXML
