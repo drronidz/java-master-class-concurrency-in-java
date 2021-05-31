2db4fede1ad74370181581515cb56369dfcb17da
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-06-01 00:03:48 +0100

    issue done. closes #26

--- Concurrency Challenges/src/com/drronidz/BankAccount.java
+++ Concurrency Challenges/src/com/drronidz/BankAccount.java
@@ -5,15 +5,23 @@ package com.drronidz;/*
     CREATED ON : 11:30 PM
 */
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
 
     private String accountNumber;
     private double balance;
 
+    private Lock lock;
+
+
     public BankAccount(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
+        this.lock = new ReentrantLock();
     }
 
 //    public synchronized void deposit(double amount) {
@@ -25,14 +33,67 @@ public class BankAccount {
 //    }
 
     public void deposit(double amount) {
-        synchronized (this) {
-            balance += amount;
+
+//        synchronized (this) {
+//            balance += amount;
+//        }
+
+//        lock.lock();
+//        try {
+//            balance += amount;
+//        } finally {
+//            lock.unlock();
+//        }
+        try {
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            // do something here
         }
     }
 
     public void withdraw(double amount) {
-        synchronized (this) {
-            balance -= amount;
+
+//        synchronized (this) {
+//            balance -= amount;
+//        }
+
+//        lock.lock();
+//        try {
+//            balance -= amount;
+//        } finally {
+//            lock.unlock();
+//        }
+
+        try {
+            if(lock.tryLock(1000,TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            // do something here
         }
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account number = : " + accountNumber);
     }
 }
